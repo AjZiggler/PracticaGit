369231386039473b0a7e9b28a7dab7e705beffc5
AjZiggler <irc152641@gmail.com>
2023-10-02 07:46:36 -0500

    ApuntesDeClase

---
+++ Parcial2Git/ExportEImport/Export.js
@@ -0,0 +1,13 @@
+export function areaCuadrado(lado) {
+    return lado*lado;
+}
+
+export function areaTriangulo(base,altura) {
+    return ((base*altura)/2);
+}
+
+//module.exports.areaCuadrado=areaCuadrado;
+//module.exports.areaTriangulo=areaTriangulo;
+//module.exports.LongitudCircunferencia=LongitudCircunferencia;
+
+//Los exports e imports solo se pueden utilizar en los navegadores
---
+++ Parcial2Git/ExportEImport/Import.js
@@ -0,0 +1,9 @@
+import * as Export from "./Export.js";
+let areas=require("./Export.js");
+
+let a=areas.areaCuadrado(4);
+
+let b=areas.areaTriangulo(2, 2);
+
+console.log(a);
+console.log(b);
